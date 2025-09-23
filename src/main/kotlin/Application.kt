db86792c3df8942254b40cc9126e5434a12da80f
Hangga Aji Sayekti <bazeniancode@gmail.com>
2025-09-23 13:40:43 +0700

    update application main

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,17 +1,9 @@
 package io.demo.productcatalog
 
 import io.ktor.server.application.*
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
 
-fun main() {
-//    io.ktor.server.netty.EngineMain.main()
-    embeddedServer(
-        Netty,
-        port = System.getenv("PORT")?.toIntOrNull() ?: 8080,
-        host = "0.0.0.0",
-        module = Application::module
-    ).start(wait = true)
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
